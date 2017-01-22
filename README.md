b51c1f120d7cc14e555e5350eb25eb648a71d68c
bfuisz <benedikt.fuisz@gmail.com>
2017-01-22 17:57:25 +0100

    Done I think

---
+++ README.md
@@ -0,0 +1,6 @@
+<h1>Make Your Own Card</h1>
+
+This is an exercise for the Udacity Course **Android Basics:User Interface (EU)**
+
+[Requirement Specification](https://classroom.udacity.com/courses/ud834-eu/lessons/2bdfe89e-b44c-4789-90a6-755077aff7f8/concepts/43534185690923#)
+
--- app/src/main/java/at/fibuszene/birthdaycard/MainActivity.java
+++ app/src/main/java/at/fibuszene/birthdaycard/MainActivity.java
@@ -1,13 +1,48 @@
 package at.fibuszene.birthdaycard;
 
+import android.content.res.Configuration;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView visitTxtView, cardText, adressLine, adressLine1, adressLine2;
+    private TextView visit, fromTxtView, fromNameTxtView;
+    private Typeface oldTimeyFont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Could be a CustomTextView but why bother for those couple of TextViews
+        oldTimeyFont = Typeface.createFromAsset(this.getAssets(), "fonts/oldtimey.ttf");
+
+        if (Configuration.ORIENTATION_LANDSCAPE == getResources().getConfiguration().orientation) {
+            visit = (TextView) findViewById(R.id.visit);
+            fromTxtView = (TextView) findViewById(R.id.fromTxtView);
+            fromNameTxtView = (TextView) findViewById(R.id.fromNameTxtView);
+
+            visit.setTypeface(oldTimeyFont);
+            fromTxtView.setTypeface(oldTimeyFont);
+            fromNameTxtView.setTypeface(oldTimeyFont);
+
+        } else if (Configuration.ORIENTATION_PORTRAIT == getResources().getConfiguration().orientation) {
+
+            visitTxtView = (TextView) findViewById(R.id.visitTxtView);
+            cardText = (TextView) findViewById(R.id.cardText);
+            adressLine = (TextView) findViewById(R.id.adressLine);
+            adressLine1 = (TextView) findViewById(R.id.adressLine1);
+            adressLine2 = (TextView) findViewById(R.id.adressLine2);
+
+            visitTxtView.setTypeface(oldTimeyFont);
+            cardText.setTypeface(oldTimeyFont);
+            adressLine.setTypeface(oldTimeyFont);
+            adressLine1.setTypeface(oldTimeyFont);
+            adressLine2.setTypeface(oldTimeyFont);
+
+        }
+
     }
 }
